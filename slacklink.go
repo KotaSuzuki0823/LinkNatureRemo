0a5a3d9e61131c04d3b57c8cf41fed47d1c31b73
KotaSuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2020-07-08 23:59:03 +0900

    リポジトリ変更

---
+++ slacklink.go
@@ -0,0 +1,90 @@
+package linkslack
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+var msg = ""
+
+var (
+	apitoken     = "xoxb-1229160658144-1204392688277-6vUrfmz7B6HW6XRKxBXioK9b"
+	slackChannel = "general"
+)
+
+func sendMessage(msg, apitoken, slackChannel string) error {
+	api := slack.New(apitoken)
+
+	_, _, err := api.PostMessage(slackChannel, slack.MsgOptionText(msg, false))
+
+	return err
+}
+
+func reciveMessage(apitoken, slackChannel string) string {
+	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		eventsAPI, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		switch eventsAPI.Type {
+		/*
+			Events API利用のためのURL検証
+			チャレンジリクエストに対し，リクエストボディ内JSONに含まれるChallengeの値をレスポンスボディに詰めて応答
+		*/
+		case slackevents.URLVerification:
+			var res *slackevents.ChallengeResponse
+			if err := json.Unmarshal(body, &res); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "text/plain")
+			if _, err := w.Write([]byte(res.Challenge)); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+		case slackevents.CallbackEvent:
+			innerEvent := eventsAPI.InnerEvent
+			switch event := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				message := strings.Split(event.Text, " ")
+				if len(message) < 2 {
+					w.WriteHeader(http.StatusBadRequest)
+					log.Println("Bad Request")
+					return
+				}
+				msg = message[1]
+			}
+
+		}
+	}) //func（httpヘッダ内）終了
+	return msg
+}
+
+func testsend() {
+	fmt.Println("test")
+	message := "Hello from golang"
+
+	if err := sendMessage(message, apitoken, slackChannel); err != nil {
+		log.Fatal(err)
+	}
+}
