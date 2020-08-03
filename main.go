b52118d9a5b54a55c59ed77af36082fe9d45b780
KotaSuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2020-08-04 00:46:27 +0900

    修正

--- main.go
+++ main.go
@@ -5,19 +5,27 @@ import (
 	"./nuture"
 	"log"
 	"os"
+	"github.com/gin-gonic/gin"
 )
 
 var (
 	apitoken     = os.Getenv("SLACKAPI_TOKEN")
 	slackChannel = "general"
 )
-
-func main() {
+func service(){
 	result := nuture.Curltest()
 	//nuture.Dummy()
 	err := linkslack.SendMessage(string(result), apitoken, slackChannel)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
+func main() {
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context){
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run()
 }
